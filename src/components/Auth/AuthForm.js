fa3ac76ed29f969e07b0e92d7947a167a5a4e96e
ChappaAbhiram <abhiramchappa@gmail.com>
2023-09-16 13:46:48 +0530

    Managed the login token using useContext

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,13 @@
-import { useState, useRef } from 'react';
+import { useState, useRef ,useContext } from 'react';
+import AuthContext from '../../store/auth-context';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const emailInputref = useRef();
   const passwordInputref = useRef(); 
+
+  const authctx = useContext(AuthContext);
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading,setIsLoading] = useState(false);
 
@@ -51,6 +54,7 @@ const AuthForm = () => {
     }
   }).then(data =>{
     console.log(data);
+    authctx.login(data.idToken);
   })
   .catch(err=>{
     alert(err.message);
