8d32b4c4f09829c21a6ae9fce1a457b0b771b8ba
ChappaAbhiram <abhiramchappa@gmail.com>
2023-09-16 17:59:58 +0530

    Autologout feature implemented

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 const AuthContext = React.createContext({
 token : '',
 isLoggedIn : false,
@@ -9,14 +9,29 @@ export const AuthContextProvider = (props)=>{
     const initialToken = localStorage.getItem('token');
     const [token,setToken] = useState(initialToken);
     const userisLoggedIn = !!token;
+    const tokenExpirationTime = localStorage.getItem('tokenExpirationTime');
+    let logoutTimer;
     const loginHandler = (token) =>{
+        const expirationTime = new Date().getTime() + 5 * 60 * 1000; // 5 minutes
         localStorage.setItem('token',token);
+        localStorage.setItem('tokenExpirationTime', expirationTime);
         setToken(token);
     }
+    logoutTimer = setTimeout(() => {
+        logoutHandler();
+      }, 5 * 60 * 1000); // 5 minutes
     const logoutHandler = ()=>{
         setToken(null);
         localStorage.removeItem('token');
+        localStorage.removeItem('tokenExpirationTime');
+        clearTimeout(logoutTimer); 
     }
+    useEffect(() => {
+        if (tokenExpirationTime && new Date().getTime() > tokenExpirationTime) {
+          logoutHandler();
+        }
+      }, [tokenExpirationTime]);
+    
     const contextValue = {
         token : token,
         isLoggedIn : userisLoggedIn,
